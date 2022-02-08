4099df7878ee8be508f4810f5f0242b7558bca62
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-02-08 21:16:28 +0800

    Implement onClick method

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-  CardText,
-  CardImg,
-} from "reactstrap";
+import { Card, CardBody, CardTitle, CardText, CardImg } from "reactstrap";
 
 class Planets extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedPlanet: null,
+    };
+  }
+
+  handleSelect(planet) {
+    this.setState({
+      selectedPlanet: planet,
+    });
+  }
+
   render() {
     const planets = this.props.planets.map((planet) => {
       return (
-        <div key={planet.id} className="col-md-4 mt-5">
+        <div
+          key={planet.id}
+          onClick={() => this.handleSelect(planet)}
+          className="col-md-4 mt-5"
+        >
           <Card>
             <CardImg alt={planet.name} src={planet.img} top width="100%" />
             <CardBody>
@@ -26,6 +38,31 @@ class Planets extends Component {
     return (
       <div className="container">
         <div className="row">{planets}</div>
+
+        {this.state.selectedPlanet ? (
+          <div>
+            <Card className="mt-5 mb-5">
+              <CardBody>
+                <CardTitle tag="h3">
+                  Details of {this.state.selectedPlanet.name}
+                </CardTitle>
+                <CardText>
+                  <span>
+                    <b>Number of moons:</b> {this.state.selectedPlanet.moons}<br/>
+                  </span>
+                  <span>
+                    <b>Length of year (earth days):</b>{" "}
+                    {this.state.selectedPlanet.lengthOfYear}<br/>
+                  </span>
+                  <span>
+                    <b>Distance from sun (astronomical unit):</b>{" "}
+                    {this.state.selectedPlanet.distanceFromSun}
+                  </span>
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+        ) : null}
       </div>
     );
   }
