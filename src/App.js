e26cbb48fd01fdf9e9fceb30b7f27680244edeb0
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-02-08 19:49:54 +0800

    Lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Planets from "./components/Planets";
+import { planets } from "./data";
 
-function App() {
-  return (
-    <>
-      <Navbar color="dark" dark expand="md">
-        <div className="container">
-          <NavbarBrand href="/">Planets</NavbarBrand>
-        </div>
-      </Navbar>
-      <Planets />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      planets: planets,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <div className="container">
+            <NavbarBrand href="/">Planets</NavbarBrand>
+          </div>
+        </Navbar>
+        <Planets planets={this.state.planets} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -6,20 +6,13 @@ import {
   CardText,
   CardImg,
 } from "reactstrap";
-import { planets } from "../data";
 
 class Planets extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      planets: planets,
-    };
-  }
   render() {
-    const planets = this.state.planets.map((planet) => {
+    const planets = this.props.planets.map((planet) => {
       return (
-        <div className="col-md-4 mt-5">
-          <Card key={planet.id}>
+        <div key={planet.id} className="col-md-4 mt-5">
+          <Card>
             <CardImg alt={planet.name} src={planet.img} top width="100%" />
             <CardBody>
               <CardTitle tag="h5">{planet.name}</CardTitle>
